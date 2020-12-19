1804d6e020ff2b3ea8a93364c113945176f82b51
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-12-19 12:50:03 -0700

    edit css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,25 +2,27 @@ import React from 'react';
 
 // Props are passed through our functional component.
 function NavTabs(props) {
-  const tabs = ['Projects', 'About', 'Contact', 'Resume'];
+  const tabs = ['Home', 'Projects', 'About', 'Contact', 'Resume'];
   return (
-    <ul className={`nav nav-tabs hero header`}>
-      {tabs.map(tab => (
-        <li className="nav-item" key={tab}>
-          <a
-            href={'#' + tab.toLowerCase()}
-            // Whenever a tab is clicked on,
-            // the current page is set through the handlePageChange props.
-            onClick={() => props.handlePageChange(tab)}
-            className={
-              props.currentPage === tab ? 'nav-link active' : 'nav-link'
-            }
-          >
-            {tab}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <div className="hero">
+      <ul className={`nav nav-tabs`}>
+        {tabs.map(tab => (
+          <li className="nav-item" key={tab}>
+            <a
+              href={'#' + tab.toLowerCase()}
+              // Whenever a tab is clicked on,
+              // the current page is set through the handlePageChange props.
+              onClick={() => props.handlePageChange(tab)}
+              className={
+                props.currentPage === tab ? 'nav-link active' : 'nav-link'
+              }
+            >
+              {tab}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 const Home = () => (
-  <div className="homepage">
-    <h1>Home Page</h1>
+  <div id="home" className="homepage">
+    <h1>Welcome to Brandi's React Portfolio!</h1>
     <p>
-      Take a look around!
+      Take a look around! You will see completed projects, a mini about-me, a way to contact me (through form or by links in the footer), and current resume.
     </p>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
   </div>
 );
 
